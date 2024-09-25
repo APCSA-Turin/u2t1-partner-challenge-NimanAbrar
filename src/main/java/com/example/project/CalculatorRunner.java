3ac64561489e20225486c69f3830375121a38a64
NimanAbrar <nimana2@nycstudents.net>
2024-09-25 16:06:24 +0000

    message

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -2,7 +2,12 @@ package com.example.project;
 
 public class CalculatorRunner {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator("TI84");
+        System.out.println(calculator.performOperation("+", 8, 9));
+        System.out.println(calculator.divisibleBy(9, 4));
+        System.out.println(calculator.coordinatePair(5,6));
+        System.out.println(calculator.absoluteValue(10,-4));
+        System.out.println(calculator.info());
         //test your program here
         //1. create an instance of the calculator class
 
